d9aae3ce6d56db138de4155c1c7e5fca3c151718
Devang Sharma <devang.25bai10547@vitbhopal.ac.in>
2026-09-17 14:08:28 +0530

    Create Java project structure

---
+++ src/campus/Main.java
@@ -0,0 +1,11 @@
+package campus;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("========================================");
+        System.out.println(" CAMPUS ACTIVITY & EVENT MANAGEMENT");
+        System.out.println("========================================");
+        System.out.println("Application started successfully.");
+    }
+}
